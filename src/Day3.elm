6cff386fa7e6bb0aef52039971950f9ab94c03d5
Jonathan McAuliffe <jon@birchavenue.org>
2019-12-04 08:33:45 +1100

    initial commit

---
+++ src/Day3.elm
@@ -0,0 +1,101 @@
+module Day3 exposing (Direction(..), Distance, Line, Point, parse, parseList, toLine, toLines, detectCrossing, solvePart1, solvePart2)
+
+import Tuple exposing (first, second)
+
+
+type Direction
+    = Left
+    | Right
+    | Up
+    | Down
+
+
+type alias Distance =
+    Int
+
+
+type alias Point =
+    ( Int, Int )
+
+
+type alias Line =
+    ( Point, Point )
+
+parseList: String -> List ( Direction, Distance )
+parseList s =
+    String.split "," s
+    |> List.filterMap parse
+
+-- Convert a single direction string to a typed representation
+
+
+parse : String -> Maybe ( Direction, Distance )
+parse s =
+    case String.toList s of
+        d :: num ->
+            case d of
+                'U' ->
+                    Maybe.map (\n -> Tuple.pair Up n) (String.toInt <| String.fromList num)
+
+                'D' ->
+                    Maybe.map (\n -> Tuple.pair Down n) (String.toInt <| String.fromList num)
+
+                'L' ->
+                    Maybe.map (\n -> Tuple.pair Left n) (String.toInt <| String.fromList num)
+
+                'R' ->
+                    Maybe.map (\n -> Tuple.pair Right n) (String.toInt <| String.fromList num)
+
+                _ ->
+                    Nothing
+        _ ->
+            Nothing
+
+
+toLine : Point -> ( Direction, Distance ) -> Line
+toLine point ( direction, distance ) =
+    case direction of
+        Up ->
+            ( point, ( first point, second point + distance ) )
+
+        Down ->
+            ( point, ( first point, second point - distance ) )
+
+        Left ->
+            ( point, ( first point - distance, second point ) )
+
+        Right ->
+            ( point, ( first point + distance, second point ) )
+
+toLines : Point -> List (Direction, Distance) -> List Line
+toLines  point l = case l of
+    x :: xs -> let new = (toLine point x) in new :: (toLines (second new) xs)
+    [] -> []
+
+
+
+
+detectCrossing : Line -> Line -> Maybe Point
+detectCrossing ( ( x1, y1 ), ( x2, y2 ) ) ( ( x3, y3 ), ( x4, y4 ) ) =
+  if (between x3 x4 x1 && between x3 x4 x2 && between y1 y2 y3  && between y1 y2 y4) then Just (x1, y3) else
+  if ( between y3 y4 y1  && between y3 y4 y2 && between x1 x2 x3 && between x1 x2 x4) then Just (x3, y1) else
+  Nothing
+
+
+
+between : Int -> Int -> Int -> Bool
+between a b m =
+    ((m > a) && (m < b))|| ((m > b) && (m < a))
+
+solvePart1 : (String, String) -> Maybe Int
+solvePart1 t =
+ let a = first t |> parseList |>  toLines ( 0, 0 )
+     b = second t |> parseList |>  toLines ( 0, 0 )
+ in
+ List.concatMap (\x -> List.filterMap (\y -> detectCrossing x y) a ) b
+  |> List.map (\x -> (abs (first x)) + (abs(second x)))
+  |> List.sort
+  |> List.head
+
+solvePart2 : (String, String) -> Maybe Int
+solvePart2 = solvePart1
