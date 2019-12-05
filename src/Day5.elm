d69deb30b7c03352252f93a09be48ead1a3085c0
Jonathan McAuliffe <jon@birchavenue.org>
2019-12-06 07:11:34 +1100

    WIP

--- src/Day5.elm
+++ src/Day5.elm
@@ -1,6 +1,9 @@
 module Day5 exposing (..)
 
 import Array exposing (..)
+
+
+
 -- https://adventofcode.com/2019/day/5
 
 
@@ -15,26 +18,47 @@ type alias Address =
 type alias Value =
     Int
 
-type alias AddressDecoder = Address -> Program -> Address
 
-directDecoder: AddressDecoder
-directDecoder a _ = a
+type alias AddressDecoder =
+    Address -> Program -> Address
 
-indirectDecoder: AddressDecoder 
-indirectDecoder a p = getVal a p
 
-addressModeDecoder m = if m == 1 then directDecoder else indirectDecoder
+directDecoder : AddressDecoder
+directDecoder a _ =
+    a
 
-instructionDecoder: Value -> (Value, (AddressDecoder, AddressDecoder, AddressDecoder))
+
+indirectDecoder : AddressDecoder
+indirectDecoder a p =
+    Maybe.withDefault -1 <| Array.get a p
+
+
+addressModeDecoder m =
+    if m == 1 then
+        directDecoder
+
+    else
+        indirectDecoder
+
+
+instructionDecoder : Value -> ( Value, ( AddressDecoder, AddressDecoder, AddressDecoder ) )
 instructionDecoder inst =
-  let i = String.fromInt inst |> String.padLeft 5 '0' |> String.toList |> List.filterMap (\x -> String.toInt (String.fromChar x)) in
-  case i of
-  a :: b :: c :: d :: e :: [] -> ((10 * d) + e, (addressModeDecoder c, addressModeDecoder b, addressModeDecoder a))
-  _ -> (99, (directDecoder, directDecoder, directDecoder))    
+    let
+        i =
+            String.fromInt inst |> String.padLeft 5 '0' |> String.toList |> List.filterMap (\x -> String.toInt (String.fromChar x))
+    in
+    case i of
+        a :: b :: c :: d :: e :: [] ->
+            ( (10 * d) + e, ( addressModeDecoder c, addressModeDecoder b, addressModeDecoder a ) )
+
+        _ ->
+            ( 99, ( directDecoder, directDecoder, directDecoder ) )
 
 
 
 -- Parse input string into list of ints
+
+
 parse : String -> Program
 parse s =
     String.split "," s
@@ -42,49 +66,38 @@ parse s =
         |> Array.fromList
 
 
-getVal : Address -> Program -> Value
-getVal i program =
-    Maybe.withDefault -1 <| Array.get i program
+getVal : Address -> AddressDecoder -> Program -> Value
+getVal a d p =
+    p |> Array.get (d a p) |> Maybe.withDefault -1
 
 
-setVal : Address -> Value -> Program -> Program
-setVal i v program =
-    program |> Array.set i v
-
-
-indirectGetVal : Address -> Program -> Value
-indirectGetVal i program =
-    getVal (getVal i program) program
-
-
-indirectSetVal : Address -> Value -> Program -> Program
-indirectSetVal i v program =
-    setVal (getVal i program) v program
+setVal : Address -> AddressDecoder -> Value -> Program -> Program
+setVal a d v p =
+    p |> Array.set (d a p) v
 
 
 
 -- Recursive program instruction decode/execute
 
 
-run : Maybe Value -> Maybe Value -> Address -> Program -> (Program, Maybe Value)
-
+run : Maybe Value -> List Value -> Address -> Program -> ( Program, List Value )
 run input output pc program =
-    case instructionDecoder(getVal pc program) of
-        (1, (d1, d2, d3)) ->
-            indirectSetVal (pc + 3) (indirectGetVal (pc + 1) program + indirectGetVal (pc + 2) program) program
+    case instructionDecoder (getVal pc directDecoder program) of
+        ( 1, ( d1, d2, d3 ) ) ->
+            setVal (pc + 3) d3 (getVal (pc + 1) d1 program + getVal (pc + 2) d2 program) program
                 |> run input output (pc + 4)
 
-        (2, (d1, d2, d3))  ->
-            indirectSetVal (pc + 3) (indirectGetVal (pc + 1) program * indirectGetVal (pc + 2) program) program
+        ( 2, ( d1, d2, d3 ) ) ->
+            setVal (pc + 3) d3 (getVal (pc + 1) d1 program * getVal (pc + 2) d2 program) program
                 |> run input output (pc + 4)
-        (3, (d1, _, _)) -> 
-            Maybe.withDefault program (Maybe.map (\i -> indirectSetVal (pc + 1) i program) input) |> run input output (pc + 2)  
-        (4, (d1, _, _)) -> 
-            run input (Just (indirectGetVal (pc + 1) program)) (pc + 2) program 
+
+        ( 3, ( d1, _, _ ) ) ->
+            Maybe.withDefault program (Maybe.map (\i -> setVal (pc + 1) d1 i program) input) |> run input output (pc + 2)
+
+        ( 4, ( d1, _, _ ) ) ->
+            -- if non-zero output halt for debugging
+            let o = ((getVal (pc + 1) d1 program)) in
+            if o > 0 then ( program, o:: output ) else run input (o :: output) (pc + 2) program
+
         _ ->
-            (program, output)
-
-
-
-
-
+            ( program, output )
